dac2896f3ac4e94a7ce998a86b615ad8072ac197
Rasoul Azizi <rasoulazizi@outlook.com>
2025-12-05 17:53:58 +0330

    fix(tickets): recreate with true diction name

--- config/settings.py
+++ config/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'accounts',
     'core',
     'events',
-    'tikets',
+    'tickets',
 ]
 
 
--- tikets/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TiketsConfig(AppConfig):
-    name = 'tikets'
