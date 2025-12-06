029306018acf287b9dcda741219473dba63f1555
Rasoul Azizi <rasoulazizi@outlook.com>
2025-12-06 20:12:50 +0330

    feat(events): event viewset

--- events/views.py
+++ events/views.py
@@ -6,3 +6,11 @@ class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(organizer=self.request.user)
