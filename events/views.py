da1ac35224188ff2f47753d96212d7baedf824da
Rasoul Azizi <rasoulazizi@outlook.com>
2025-12-06 20:12:16 +0330

    feat(events): categoryviewset

--- events/views.py
+++ events/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Category, Event
+from .serializers import CategorySerializer, EventSerializer
 
-# Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
