bcfb7ace0942fde42f5087d114fa304cb71d4fe6
Rasoul Azizi <rasoulazizi@outlook.com>
2025-12-07 14:27:01 +0330

    feat(tickets): add orderserilizer

--- tickets/serializers.py
+++ tickets/serializers.py
@@ -6,3 +6,17 @@ class TicketTypeSerializer(serializers.ModelSerializer):
         model = TicketType
         fields = '__all__'
 
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'
+        read_only_fields = ['id', 'buyer', 'total_price', 'status', 'created_at', 'updated_at']
+
+    def validate(self, attrs):
+        ticket_type = attrs['ticket_type']
+        quantity = attrs['quantity']
+
+        if ticket_type.capacity < quantity:
+            raise serializers.ValidationError({"quantity": "Not enough tickets available."})
+        
+        return attrs
