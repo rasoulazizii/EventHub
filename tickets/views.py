f1f4748ac70a4c1ade3fd3e02c35a7cd333fe4f0
Rasoul Azizi <rasoulazizi@outlook.com>
2025-12-09 08:24:05 +0330

    feat(tickets): add transaction db and loking(perform_create)

--- tickets/views.py
+++ tickets/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, permissions
+from django.db import transaction
+from rest_framework import viewsets, permissions, serializers
 from .models import TicketType, Order
 from .serializers import TicketTypeSerializer, OrderSerializer
 
@@ -15,13 +16,25 @@ class OrderViewSet(viewsets.ModelViewSet):
         return Order.objects.filter(buyer=self.request.user)
 
     def perform_create(self, serializer):
-        ticket_type = serializer.validated_data['ticket_type']
+        ticket_type_input = serializer.validated_data['ticket_type']
         quantity = serializer.validated_data['quantity']
-        price = ticket_type.price
-        total = price * quantity
         
-        serializer.save(
-            buyer=self.request.user,
-            total_price=total,
-            status='pai'
-        )
+        with transaction.atomic():
+            ticket = TicketType.objects.select_for_update().get(id=ticket_type_input.id)
+            
+            if ticket.capacity < quantity:
+                raise serializers.ValidationError(
+                    {"quantity": "Not enough tickets available. Someone just bought the last ones!"}
+                )
+            
+            ticket.capacity -= quantity
+            ticket.save()
+            
+            price = ticket.price
+            total = price * quantity
+            
+            serializer.save(
+                buyer=self.request.user,
+                total_price=total,
+                status='pai'
+            )
