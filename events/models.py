8a144ed915c4070feec842aed1d9894d12315d70
Rasoul Azizi <rasoulazizi@outlook.com>
2025-12-09 08:45:51 +0330

    fix(models,urls): some bugs fix

--- events/models.py
+++ events/models.py
@@ -17,19 +17,23 @@ class Category(AbstractModel):
         return self.name
 
 class Event(AbstractModel):
-    CHOICES = [
-        ['d', 'Draft'],
-        ['p', 'Published'],
-        ['c', 'canceled'],
-    ]
-    organizer = models.ForeignKey(User, on_delete=models.PROTECT, related_name='organizer')
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='Event')
+    class Status(models.TextChoices):
+        DRAFT = 'd', 'Draft'
+        PUBLISHED = 'p', 'Published'
+        CANCELED = 'c', 'Canceled'
+
+    organizer = models.ForeignKey(User, on_delete=models.PROTECT, related_name='organized_events')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='events')
     info = models.TextField()
-    start_dates = models.DateTimeField()
-    end_dates = models.DateTimeField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()  
     location = models.CharField(max_length=220)
-    status = models.CharField(max_length=1, choices=CHOICES, default='d')
+    
+    status = models.CharField(
+        max_length=1, 
+        choices=Status.choices, 
+        default=Status.DRAFT
+    )
 
     def __str__(self):
         return self.info
-
